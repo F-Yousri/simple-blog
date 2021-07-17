adf1892fc282e80dc5ad72c2e5d220ae11bb4f64
Fahd Yousri <fahdyousri@gmail.com>
2021-07-17 03:50:16 +0200

    create post table, model and factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'body' => $this->faker->text(1000), // Can take up to 65535 but using 1000 for example
+
+        ];
+    }
+
+}
