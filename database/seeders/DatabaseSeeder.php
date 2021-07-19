64d26fcba0a3cde1383cdc658de317207b3ebf82
Fahd Yousri <fahdyousri@gmail.com>
2021-07-19 17:44:08 +0200

    add dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(['email' => 'admin@example.com'])->create();
+        User::factory()->has(Post::factory())->count(100)->create();
     }
 }
