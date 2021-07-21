668e6309e5dbf9a2421f8ba0a818f790716a97e4
Fahd Yousri <fahdyousri@gmail.com>
2021-07-22 00:50:37 +0200

    add the functionality to edit users

--- app/Http/Livewire/EditUser.php
+++ app/Http/Livewire/EditUser.php
@@ -14,6 +14,29 @@ class EditUser extends Component
         $this->user = $user;
     }
 
+    public function rules()
+    {
+        return [
+            'user.name' => 'required',
+            'user.email' => 'required|email|unique:users,email,'. $this->user->id,
+        ];
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function update()
+    {
+        $validatedData = $this->validate();
+
+        $this->user->update($validatedData);
+        request()->session()->flash('flash.banner', 'User Updated successfuly!');
+        request()->session()->flash('flash.bannerStyle', 'success');
+        return redirect()->route('users.edit', ['user' => $this->user->id]);
+    }
+
     public function render()
     {
         return view('livewire.edit-user');
--- resources/views/livewire/edit-user.blade.php
+++ resources/views/livewire/edit-user.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{$user->name}}
+    <form class="flex flex-col space-y-5" wire:submit.prevent="update">
+
+        <!-- Name -->
+        <div class="col-span-6 sm:col-span-4">
+        <x-jet-label for="name" value="{{ __('Name') }}" />
+        <x-jet-input id="name" type="text" class="block w-full mt-1" wire:model="user.name" autocomplete="name" />
+        <x-jet-input-error for="user.name" class="mt-2" />
+        </div>
+
+        <!-- Email -->
+        <div class="col-span-6 sm:col-span-4">
+        <x-jet-label for="email" value="{{ __('Email') }}" />
+        <x-jet-input id="email" type="email" class="block w-full mt-1" wire:model="user.email" />
+        <x-jet-input-error for="user.email" class="mt-2" />
+        </div>
+        <div class="flex justify-end w-full">
+            <button type="submit" class="inline-flex items-center p-3 px-8 py-3 text-white bg-indigo-600 border border-transparent rounded-full shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Update
+                <!-- Heroicon name: outline/plus -->
+                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                </svg>
+            </button>
+        </div>
+    </form>
 </div>
